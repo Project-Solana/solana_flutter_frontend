e56ab5ff3f50fe8f24bbeb4e7de21efe0d4ddaf3
Gagandeep <gagandeepkaur1523@gmail.com>
2022-09-04 13:05:31 +0530

    Fixed errors and removed unnecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import './screens/Transaction Details/transaction_details_screen.dart';
 import './screens/payment/payment_screen.dart';
 import './screens/theme.dart';
 import './screens/my_profile/profile_page.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import './screens/homepage/home_page.dart';
-
 import 'screens/Settings/settings_screen.dart';
 
 void main() => runApp(MyApp());
@@ -16,8 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: HomePage(), //AuthScreen(),
       routes: {
-        HomePage.routName: (context) => HomePage(),
-        SettingsScreen.routebase: (ctx) => SettingsScreen(),
+        HomePage.routeName: (context) => HomePage(),
+        SettingsScreen.routeName: (ctx) => SettingsScreen(),
         ProfilePage.routeName: (context) => ProfilePage(),
         PaymentScreen.routeName: (context) => PaymentScreen(),
         DarkLight.routeName: (context) => DarkLight(),
